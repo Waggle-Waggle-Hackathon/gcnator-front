1f2afbdb79ab351285f7d972f4fc2ea31ee2dee3
dobby <dobbidd0@gmail.com>
2023-08-18 23:51:53 +0900

    feat: user edittext

--- Gachonator/app/src/main/java/com/example/gachonator/UserActivity.kt
+++ Gachonator/app/src/main/java/com/example/gachonator/UserActivity.kt
@@ -3,10 +3,19 @@ package com.example.gachonator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
 import com.example.gachonator.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity() {
     lateinit var binding: ActivityUserBinding
+
+    var major = ""
+    var stdId = ""
+    var name = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserBinding.inflate(layoutInflater)
@@ -14,8 +23,66 @@ class UserActivity : AppCompatActivity() {
 
         initListener()
     }
+
+    override fun onResume() {
+
+        class myTextWatcher(var id: Int) : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+            override fun afterTextChanged(s: Editable?) {
+                when(id){
+                    1 -> {
+                        major = binding.userMajorEnterEt.text.toString()
+                    }
+                    2 -> {
+                        stdId = binding.userIdEnterEt.text.toString()
+                    }
+                    3 -> {
+                        name = binding.userNameEnterEt.text.toString()
+                    }
+                    else -> Log.d("User Activity", "Input Error")
+                }
+
+                Log.d("major stdId name", major + " " + stdId + " " + name)
+
+                // 세개 다 입력값 있으면 버튼 활성화
+                if (major!="" && stdId!="" && name!=""){
+                    binding.userNextBtnOffTv.visibility = View.GONE
+                    binding.userNextBtnOnTv.visibility = View.VISIBLE
+                } else {
+                    binding.userNextBtnOffTv.visibility = View.VISIBLE
+                    binding.userNextBtnOnTv.visibility = View.GONE
+                }
+            }
+
+        }
+
+        // 학과 EditText listen
+        var mWatcher = myTextWatcher(1)
+        binding.userMajorEnterEt.addTextChangedListener(mWatcher)
+
+        // 학번 EditText listen
+        var iWatcher = myTextWatcher(2)
+        binding.userIdEnterEt.addTextChangedListener(iWatcher)
+
+        // 이름 EditText listen
+        var nWatcher = myTextWatcher(3)
+        binding.userNameEnterEt.addTextChangedListener(nWatcher)
+
+        initListener()
+
+        super.onResume()
+    }
+
     private fun initListener() {
-        binding.userNextBtnTv.setOnClickListener {
+        binding.userNextBtnOnTv.setOnClickListener {
+
+            /** 서버에 결과 전송 */
+
             startActivity(Intent(this, QuestionActivity::class.java))
         }
     }
